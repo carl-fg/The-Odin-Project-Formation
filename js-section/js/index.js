c9395091c4ad997e2c64c79a1f8acd4a3679496f
carl <carl.fischergode91470@gmail.com>
2026-05-26 08:11:58 +0200

    Initialisation de la formation JS

---
+++ js-section/js/index.js
@@ -0,0 +1,16 @@
+console.log("Hello World !");
+
+const pi = Math.PI
+console.log("Pi =", pi)
+
+// CONDITIONS IF / ELSE / ELSE IF
+
+// let budget = prompt("Quel est votre budget ?");
+
+// if (budget < 200000){
+//     alert("Le budget est trop restreint.")
+// } else if (budget > 500000){
+//     alert("Vous pouvez viser un meilleur bien immobilier.")
+// } else{
+//     alert("Très bien !")
+// }
