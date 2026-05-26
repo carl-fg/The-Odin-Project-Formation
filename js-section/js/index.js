468c7ca8c2d201efb90116c84222cb6c7dfa6b37
carl <carl.fischergode91470@gmail.com>
2026-05-26 16:51:12 +0200

    Début du projet pierre papier ciseaux

--- js-section/js/index.js
+++ js-section/js/index.js
@@ -1,16 +1,81 @@
-console.log("Hello World !");
+// FONCTION ADDITIONER AVEC 7
+function add7(num){
 
-const pi = Math.PI
-console.log("Pi =", pi)
+    let final = num + 7;
+    console.log(final);
+    return final;
+}
 
-// CONDITIONS IF / ELSE / ELSE IF
+// let choice = parseFloat(prompt("Quelle chiffre de base ?"));
+// // add7(choice);
 
-// let budget = prompt("Quel est votre budget ?");
 
-// if (budget < 200000){
-//     alert("Le budget est trop restreint.")
-// } else if (budget > 500000){
-//     alert("Vous pouvez viser un meilleur bien immobilier.")
-// } else{
-//     alert("Très bien !")
-// }
+// FONCTION MULTIPLIER
+function multiply(num2, num3){
+
+    let final2 = num2 * num3;
+    console.log(final2);
+    return final2;
+}
+
+// let choice2 = parseFloat(prompt("Quelle chiffre de base ?"));
+// let choice3 = parseFloat(prompt("Quelle chiffre pour le multiplier ?"));
+// multiply(choice2, choice3);
+
+
+// FONCTION CAPITALIZE
+function capitalize(text){
+
+    let correctSentence = text.charAt(0).toUpperCase() + text.slice(1);
+    console.log(`La phrase de base était "${sentence}" et maintenant : ${correctSentence}`);
+    return correctSentence;
+}
+
+// let sentence = "je vais manger dehors.";
+// capitalize(sentence);
+
+
+// FONCTION LASTLETTER
+function lastLetter(word){
+
+    let last = word.slice(-1);
+    console.log(last);
+    return last;
+    
+}
+
+// const wds = "Banane";
+// lastLetter(wds);
+
+// FONCTION PIERRE PAPIER CISEAUX
+
+const choices = ["pierre", "papier", "ciseaux"];
+
+function getComputerChoice(items){
+
+    let botChoice = items[Math.floor(Math.random() * items.length)];
+    console.log(`L'ordinateur fait ${botChoice}.`)
+    return botChoice;
+}
+
+function getHumanChoice(){
+
+    let humanChoice = prompt("Choisissez entre 'pierre', 'papier' ou 'ciseaux'.");
+
+    if (humanChoice == "pierre"){
+        console.log("Vous avez choisi pierre.");
+        
+    } else if (humanChoice == "papier"){
+        console.log("Vous avez choisi papier.");
+        
+    } else if (humanChoice == "ciseaux"){
+        console.log("Vous avez choisi ciseaux.");
+        
+    } else {
+        console.log("Votre choix n'a pas de correspondance. Vérifiez l'orthographe.");
+        
+    }
+}
+
+getComputerChoice(choices)
+getHumanChoice()
